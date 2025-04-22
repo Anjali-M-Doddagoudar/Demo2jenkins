3519fb8112981ffe15544df4c889d52ca38ea017
Anjali-M-Doddagoudar <anjalimdoddagoudar548@gmail.com>
2025-04-22 11:10:02 +0530

    Save it in master file

---
+++ Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+
+    public static void main(String[] args) {
+        // Hardcoded employee details
+        String name = "John Doe";
+        int id = 101;
+        String department = "Engineering";
+        double salary = 75000.00;
+        int phoneno = 987695843;
+
+        // Displaying employee details
+        System.out.println("=== Employee Details ===");
+        System.out.println("Name       : " + name);
+        System.out.println("ID         : " + id);
+        System.out.println("Department : " + department);
+        System.out.println("Salary     : $" + salary);
+        System.out.println("Phone Number :" + phoneno);
+    }
+}
